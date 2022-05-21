58eed183700110a98a831309e9553f5d12ce6027
ggolish <ggolish2@gmail.com>
2022-05-21 19:28:27 -0400

    Initial git implementation.
    - Create repo if it does not exist at $BR
    - Set up worktree project, allow selecting worktree.

--- .config/workon/utils/git.sh
+++ .config/workon/utils/git.sh
@@ -1,3 +1,48 @@
+# Variables that MUST be set:
+# BR => The profile root will be used as the destination of the git repo.
+# WORKON_GIT_REMOTE => A remote git URL for the repo that will be used to fetch
+#   the repo if it does not exist at $BR
+#
+# Variables that CAN be set:
+# WORKON_GIT_WORKTREES => Enables worktree mode. It will assume that $BR points
+#   to a directory containing subdirectories that are actually worktrees of the
+#   repo, where $BR/master contains the actual repo itself. Then the user will
+#   be prompted to choose a worktree, which will be appended to $BR.
+
+function __fetch_repo {
+    if [[ ! -z "$WORKON_GIT_WORKTREES" ]]; then
+        mkdir -p "$BR"
+        git clone "$WORKON_GIT_REMOTE" "$BR/master"
+        return
+    fi
+    git clone "$WORKON_GIT_REMOTE" "$BR"
+}
+
+function __worktree_select {
+    choice=$(ls -1 "$BR" | fzf --prompt "Choose worktree: ")
+
+    # Default to the master branch
+    [[ -z "$choice" ]] && choice="master"
+
+    if [[ ! -d "$BR/$choice" ]]; then
+        echo "worktree '$BR/$choice' does not exist, skipping"
+        return
+    fi
+
+    BR="$BR/$choice"
+}
+
 function __env_activate {
-    echo "activating git env"
+    if [[ -z "$BR" ]] || [[ -z "$WORKON_GIT_REMOTE" ]]; then
+        return
+    fi
+
+    if [[ ! -d "$BR" ]]; then
+        if ! __fetch_repo; then
+            echo "failed to fetch git repo $WORKON_GIT_REMOTE"
+            return
+        fi
+    fi
+
+    [[ ! -z "$WORKON_GIT_WORKTREES" ]] && __worktree_select
 }
