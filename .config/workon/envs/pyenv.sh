f487cb86aaec546ae0277dadc2506696576233ec
ggolish <ggolish2@gmail.com>
2022-05-20 23:51:58 -0400

    Initialized pyenv env.

--- .config/workon/envs/pyenv.sh
+++ .config/workon/envs/pyenv.sh
@@ -1,4 +1,37 @@
+# Variables that MUST be set:
+# WORKON_PYENV_VIRTUALENV => The desired virtualenv name
+#
+# Variables that CAN be set:
+# WORKON_PYENV_VERSION => The desired python version for the virtualenv
+
+function __ensure_pyenv {
+    [[ ! -z $(which pyenv 2> /dev/null) ]] && [[ ! -z $PYENV_VIRTUALENV_INIT ]]
+}
+
+function __ensure_pyenv_virtualenv {
+    if [[ ! -z $(pyenv virtualenvs | awk '{print $1}' | grep $WORKON_PYENV_VIRTUALENV) ]]; then
+        return
+    fi
+
+    if [[ -z "$WORKON_PYENV_VERSION" ]]; then
+        pyenv virtualenv $WORKON_PYENV_VIRTUALENV
+    else
+        if [[ -z $(pyenv versions | awk '{print $1}' | grep $WORKON_PYENV_VERSION) ]]; then
+            pyenv install "$WORKON_PYENV_VERSION"
+        fi
+        pyenv virtualenv $WORKON_PYENV_VERSION $WORKON_PYENV_VIRTUALENV
+    fi
+}
+
 function __env_activate {
-    echo "activating pyenv env"
+    if [[ -z "$WORKON_PYENV_VIRTUALENV" ]]; then
+        return
+    fi
+    if ! __ensure_pyenv; then
+        echo "pyenv is not initialized"
+        return
+    fi
+    __ensure_pyenv_virtualenv
+    pyenv activate $WORKON_PYENV_VIRTUALENV
 }
 
