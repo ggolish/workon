24c21ad0967c497e5d1db5ce3417647fb8f5802c
ggolish <ggolish2@gmail.com>
2022-05-21 23:21:22 -0400

    Add clean method to defaults.

--- .config/workon/defaults/env.sh
+++ .config/workon/defaults/env.sh
@@ -1,3 +1,7 @@
 function __env_activate {
     echo "hello, env!"
 }
+
+function __env_clean {
+    echo "goodbye, env!"
+}
--- .config/workon/defaults/profile.sh
+++ .config/workon/defaults/profile.sh
@@ -1,3 +1,7 @@
 function __profile_launch {
     echo "hello, profile!"
 }
+
+function __profile_clean {
+    echo "goodbye, profile!"
+}
