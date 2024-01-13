c0acf5c7172585eee10877b23d6071c0b37cc7fa
ggolish <ggolish2@gmail.com>
2024-01-13 14:53:35 -0500

    Add stable keyword to go util.

--- .config/workon/utils/go.sh
+++ .config/workon/utils/go.sh
@@ -2,17 +2,44 @@
 # WORKON_GO_VERSION => The version of Go to be used for the profile.
 
 function __util_activate {
+
+    function ensure {
+        if ! command -v "$1" >/dev/null 2>&1; then
+            echo "$1 not found, installing..."
+            $2 $1
+        fi
+    }
+
+    function install_restish {
+        go install github.com/danielgtaylor/restish@latest
+    }
+
+    function install_gocmd {
+        GOPROXY="" go install golang.org/dl/$1@latest
+    }
+
     [[ -z "$WORKON_GO_VERSION" ]] && return
-    local gocmd="go$WORKON_GO_VERSION"
-    if ! command -v "$gocmd" &> /dev/null; then
-        echo "Installing $gocmd..."
-        GOPROXY="" go install golang.org/dl/$gocmd@latest || return
+
+    local gocmd=""
+    if [[ "$WORKON_GO_VERSION" == "stable" ]]; then
+        ensure restish install_restish
+        gocmd=$(restish https://go.dev/dl/?mode=json -f "body[0].version" -r)
+    else
+        gocmd="go$WORKON_GO_VERSION"
     fi
+
+    ensure $gocmd install_gocmd
+
     if ! $gocmd version &> /dev/null; then
         echo "Downloading $gocmd..."
-        $gocmd download || return
+        $gocmd download
     fi
+
     alias go="$gocmd"
+
+    unset ensure
+    unset install_restish
+    unset install_gocmd
 }
 
 function __util_clean {
